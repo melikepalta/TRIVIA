c5c6751a6ed31ebf83636228585a8fbb7beb5538
melikepalta <122238906+melikepalta@users.noreply.github.com>
2024-08-29 15:59:57 -0400

    Update app.js
    modified app.js to add the new quiz route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const startPointRoute = require('./app/start-point/start-point.route.js');
+const quizRoute = require('./app/quiz/quiz.route'); //imported the quiz route
 
 // Setup Handlebars as the view engine
 app.set('view engine', 'hbs');
@@ -9,6 +10,7 @@ app.set('views', path.join(__dirname, 'app'));
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/quiz', quizRoute); //used the quiz route
 
 // Use the start-point route
 app.use('/start-point', startPointRoute);
