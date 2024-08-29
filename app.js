dad056858a84da48d3b74c65f1de28a85746256b
melikepalta <122238906+melikepalta@users.noreply.github.com>
2024-08-29 17:22:49 -0400

    Revert "Task2 questions functionality"

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const startPointRoute = require('./app/start-point/start-point.route.js');
-const quizRoute = require('./app/quiz/quiz.route.js'); //imported the quiz route
 
 // Setup Handlebars as the view engine
 app.set('view engine', 'hbs');
@@ -13,7 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Use the start-point route
 app.use('/start-point', startPointRoute);
-app.use('/quiz', quizRoute); //used the quiz route
 
 // Start the server
 const PORT = process.env.PORT || 3000;
--- app/quiz/quiz.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const quizController = require('./quiz.controller');
-
-router.get('/', quizController.renderQuiz);
-
-module.exports = router;
