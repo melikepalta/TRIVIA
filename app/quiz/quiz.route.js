e4a930a6393fd3e60fe24a7561db1edf4e8e61f9
melikepalta <122238906+melikepalta@users.noreply.github.com>
2024-09-19 13:57:24 -0400

    Update quiz.route.js

--- app/quiz/quiz.route.js
+++ app/quiz/quiz.route.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const quizController = require('./quiz.controller');
+const axios = require('axios');
+
 
 router.get('/', quizController.renderQuiz);
+
+router.get('/question/:id', quizController.getQuestion); // Fetches each question
+router.post('/finish', quizController.finishQuiz); // Handles quiz completion
 
 module.exports = router;
